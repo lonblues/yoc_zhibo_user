38140223224ee316144cf055db035d10ba1c7f01
hzy <zjobe4z@dingtalk.com>
2020-07-28 14:19:06 +0800

    首次提交代码

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    camelcase: [0, {
+      properties: 'always'
+    }],
+    'no-console': process.env.NODE_ENV === 'production' ? 'off' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'off' : 'off'
+  }
+}
