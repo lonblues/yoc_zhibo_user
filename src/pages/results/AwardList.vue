00d1a5505dd1bd02af8385a5883517e1b5c2039c
王振 <1026054375@qq.com>
2020-08-10 22:32:02 +0800

    添加同一个组

--- src/pages/results/AwardList.vue
+++ src/pages/results/AwardList.vue
@@ -34,34 +34,74 @@ export default {
       award: {
         tHead: [
           { id: 'student_name', label: '名字' },
+          { id: 'group', label: '组' },
           { id: 'student_gender', label: '性别' },
           { id: 'student_mobile', label: '手机号码' },
-          { id: 'student_email', label: '邮箱' }
+          { id: 'student_email', label: '邮箱' },
+          { id: 'awardName', label: '奖项类型' },
+          { id: 'test_award', label: '奖' }
         ],
         tBody: []
+      },
+      groups: {
+
       }
     }
   },
-  created () {
-    getAwardsByAccountProject(
-      this.$route.params.id,
-      this.$route.params.code
-    ).then(({ data }) => {
-      this.award.tBody = data.data.map(item => {
-        return {
-          ...item.student_info,
-          item
-        }
+  async created () {
+    await this.getAwardsByAccountProject()
+    await this.addGroup()
+  },
+  methods: {
+    getAwardsByAccountProject () {
+      return getAwardsByAccountProject(
+        this.$route.params.id,
+        this.$route.params.code
+      ).then(({ data }) => {
+        this.award.tBody = data.data.map(item => {
+          return {
+            ...item.student_info,
+            ...item,
+            ...item.app_info
+          }
+        })
+        this.award.tBody = this.award.tBody.map(item => {
+          return {
+            ...item,
+            student_name: `${item.student_lastName}${item.student_givenName}`,
+            awardName: item.award_type === 'team' ? '团队奖项' : '个人奖项',
+            group: item.application_team_code
+          }
+        })
       })
-      this.award.tBody = this.award.tBody.map(item => {
+    },
+    unique () {
+      const isGroups = this.award.tBody.map((item) => {
+        return item.application_team_code
+      })
+      console.log(isGroups)
+      return Array.from(new Set(isGroups))
+    },
+    addGroup () {
+      const arr = this.unique()
+      console.log(arr)
+      arr.forEach((item, index) => {
+        this.$set(this.groups, item, [])
+      })
+      this.award.tBody.forEach((item) => {
+        this.groups[item.application_team_code].push(item.student_name)
+      })
+      arr.forEach((item, index) => {
+        this.groups[item] = Array.from(new Set(this.groups[item]))
+      })
+      this.award.tBody = this.award.tBody.map((item) => {
         return {
           ...item,
-          student_name: `${item.student_lastName}${item.student_givenName}`
+          group: this.groups[item.application_team_code].join(' ')
         }
       })
-    })
-  },
-  methods: {}
+    }
+  }
 }
 </script>
 
